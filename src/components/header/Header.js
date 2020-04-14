6ba6c52e475b5b4a51b1df838ec9db7a12500a25
Robert Herciu <herciu.robert@gmail.com>
2020-04-14 13:49:44 +0300

    make it better

---
+++ src/components/header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Header(props) {
+    const a = 'Robert';
+    return (
+        <header>
+            <h1>EventPlanner/Logo</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a>Profile</a></li>
+                </ul>
+            </nav>
+            <section>
+                Welcome, { props.user } { a } !
+            </section>
+        </header>
+    );
+}
+
+export default Header;
