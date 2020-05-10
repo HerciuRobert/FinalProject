43a683567846a04b9d2a4a08d25cbb8d86902ba2
Robert Herciu <herciu.robert@gmail.com>
2020-05-10 15:44:52 +0300

    remake

--- src/components/shared/SideNav.js
+++ src/components/shared/SideNav.js
@@ -1,47 +1,48 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useLocation, useHistory, Link } from 'react-router-dom';
-import AuthContext from '../auth/AuthContext';
-import axios from 'axios';
 
+import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import '../App.css';
+import AuthContext from '../auth/AuthContext';
+
+// import ReserveRestaurant from '../restaurants/ReserveRestaurant';
+// import ReserveBand from '../bands/ReserveBand';
+// import ReserveOther from '../other/ReserveOther';
+import axios from 'axios';
 
 
 function SideNav() {
     const { auth } = useContext(AuthContext);
 
     let [reservedRestaurant, setReservedRestaurant] = useState([]);
-    // const [participantsNumber, setParticipantsNumber] = useState('');
 
-    const history = useHistory();
-    const location = useLocation();
-    const { from } = location.state || { from: { pathname: "/" } };
-
-
-
-    async function getRestaurantReservationByUserId(id) {
-        const reservation = await axios.get('/reservations/?user_email=' + auth).then(res => res.data);
-        console.log(reservation)
-        setReservedRestaurant(reservation);
-        history.replace(from);
-
-    }
 
     useEffect(() => {
         getRestaurantReservationByUserId(auth);
     }, [auth]);
 
-    function updateEventName(newName) {
-        const res =  axios('/reservations/' + reservedRestaurant[0].id, {
-            method: 'PATCH',
-            data: {
-                'event_name':newName,
-            },
-        })
+    async function getRestaurantReservationByUserId(id) {
+        const reservation = await axios.get('/reservations/?user_email=' + auth).then(res => res.data);
+        console.log(reservation)
+        setReservedRestaurant(reservation);
     }
 
-    async function handleDeleteEvent(e) {
-     await axios.delete('/reservations/' + reservedRestaurant[0].id )
-     window.location.reload();
+    function updateEventName() {
+        const res = axios('/reservations/' + reservedRestaurant[0].id, {
+            method: 'PATCH',
+            data: {
+                'event_name': getNewName(),
+            },
+        })
+        window.location.reload();
+    }
+
+    function getNewName() {
+        return document.getElementById('edit-name').value;
+    }
+
+    async function handleDelete(e) {
+        await axios.delete('/reservations/' + reservedRestaurant[0].id)
+        window.location.reload();
     }
 
     return (
@@ -51,64 +52,69 @@ function SideNav() {
                     <article className="side-nav">
                         <div>
                             {reservedRestaurant.map((list) =>
-                                <h1 onChange={getRestaurantReservationByUserId} key={`${list.event_name}`}>{list.event_name} </h1>
-                                
+                                <h1 onLoad={getRestaurantReservationByUserId} key={`${list.event_name}`}>{list.event_name}</h1>)
 
-                            )}
+                            }
 
                             {(reservedRestaurant.length && auth ?
-                                <Link className="button-style" to={"/edit-event-name"}>Edit event name</Link>
+                                <input
+                                    type="text"
+                                    id="edit-name"
+                                    className="label"
+                                /> : null)}
+                            {(reservedRestaurant.length && auth ?
+                                <a href className="button-style" onClick={updateEventName}>Edit event name</a>
                                 :
                                 null
                             )}
 
-                            {reservedRestaurant.map((list) =>
-                                <h4 onChange={getRestaurantReservationByUserId} key={`${list.date}`}>Save the date: {list.date} </h4>)
-
-                            }
-
                             <h4>Restaurant:         {reservedRestaurant.map((list) =>
-                                <div onChange={getRestaurantReservationByUserId} key={`${list.restaurantName}`}>{list.restaurantName}</div>)
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.restaurantName}`}>{list.restaurantName}</h4>)
 
                             }
                             </h4>
 
-                            {reservedRestaurant.map((list)  =>
-                                <p onChange={getRestaurantReservationByUserId} key={`${list.restaurantPrice}`}>Restaurant expenses: {list.restaurantPrice} &euro;</p>)
+                            <p>Restaurant expenses:         {reservedRestaurant.map((list) =>
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.restaurantPrice}`}>{list.restaurantPrice}</h4>)
 
                             }
+                            </p>
 
-                            <label htmlFor="participant-number">Number of participants:</label> 
-                            <input 
-                                type="number" 
-                                id="participant-number" 
-                                className="label" 
-                                placeholder="1" 
-                                min="1"
-                                value="1"
-                            />
-                            {reservedRestaurant.map((list) =>
-                            <h4 onChange={getRestaurantReservationByUserId} key={`${list.bandName}`}>Band: {list.bandName}</h4>
-                            )}
-                            {reservedRestaurant.map((list) =>
-                            <p onChange={getRestaurantReservationByUserId} key={`${list.bandPrice}`}>Band expenses: {list.bandPrice} &euro;</p>
-                            )}
-                            {reservedRestaurant.map((list) =>
-                            <h4 onChange={getRestaurantReservationByUserId} key={`${list.otherName}`}>Activity: {list.otherName}</h4>
-                            )}
-                            {reservedRestaurant.map((list) =>
-                            <p onChange={getRestaurantReservationByUserId} key={`${list.otherPrice}`}>Activity expenses: {list.otherPrice} &euro;</p>
-                            )}
-                            {reservedRestaurant.map((list) =>
-                                <h4 onChange={getRestaurantReservationByUserId} key={`${list.restaurantPrice}+${list.bandPrice}`}>Total expenses:  &euro;</h4>)
+                            <label htmlFor="participant-number">Number of participants:</label> <input type="number" id="participant-number" className="label" placeholder="1" min="1" />
+
+                            <h4>Band:         {reservedRestaurant.map((list) =>
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.bandName}`}>{list.bandName}</h4>)
+
                             }
+                            </h4>
+
+                            <p>Band expenses:         {reservedRestaurant.map((list) =>
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.bandPrice}`}>{list.bandPrice}</h4>)
+
+                            }
+                            </p>
+                            <h4>Activity:         {reservedRestaurant.map((list) =>
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.otherName}`}>{list.otherName}</h4>)
+
+                            }
+                            </h4>
+
+                            <p>Activity expenses:         {reservedRestaurant.map((list) =>
+                                <h4 onLoad={getRestaurantReservationByUserId} key={`${list.otherPrice}`}>{list.otherPrice}</h4>)
+
+                            }
+                            </p>
+
+                            <p><strong>Total expenses: (restaurant.price*{"participant-number".value})+band.price+other.price</strong></p>
+
                             {(reservedRestaurant.length && auth ?
                                 <div>
-                                    <a className="button-style" onClick={handleDeleteEvent}>Delete event</a>
+                                    <a className="button-style" onClick={handleDelete}>Delete event</a>
                                 </div>
                                 :
                                 null
                             )}
+
                         </div>
                     </article>
                 </div>
