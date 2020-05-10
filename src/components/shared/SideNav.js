b10f79e01cd06c951cc264132bcd72fddd3c3d0d
Robert Herciu <herciu.robert@gmail.com>
2020-05-10 15:56:51 +0300

    calculate price

--- src/components/shared/SideNav.js
+++ src/components/shared/SideNav.js
@@ -45,6 +45,14 @@ function SideNav() {
         window.location.reload();
     }
 
+    function calculatePrice(){
+       let participants =  document.getElementById('participant-number').value;
+       let totalRestaurant = participants * reservedRestaurant[0].restaurantPrice;
+       let total = totalRestaurant + reservedRestaurant[0].bandPrice + reservedRestaurant[0].otherPrice;
+       console.log(total);
+       return total;
+    }
+
     return (
         <>
             {(auth ?
@@ -80,7 +88,7 @@ function SideNav() {
                             }
                             </p>
 
-                            <label htmlFor="participant-number">Number of participants:</label> <input type="number" id="participant-number" className="label" placeholder="1" min="1" />
+                            <label htmlFor="participant-number">Number of participants:</label> <input type="number" id="participant-number" className="label" placeholder="1" min="1" defaultValue="1"/>
 
                             <h4>Band:         {reservedRestaurant.map((list) =>
                                 <h4 onLoad={getRestaurantReservationByUserId} key={`${list.bandName}`}>{list.bandName}</h4>)
@@ -105,8 +113,12 @@ function SideNav() {
                             }
                             </p>
 
-                            <p><strong>Total expenses: (restaurant.price*{"participant-number".value})+band.price+other.price</strong></p>
-
+                            {(reservedRestaurant.length && auth ?
+                                <a href className="button-style" onClick={calculatePrice}>Calculate price</a>
+                                :
+                                null
+                            )}
+                            <p><strong>Total expenses: {calculatePrice}</strong></p>
                             {(reservedRestaurant.length && auth ?
                                 <div>
                                     <a className="button-style" onClick={handleDelete}>Delete event</a>
